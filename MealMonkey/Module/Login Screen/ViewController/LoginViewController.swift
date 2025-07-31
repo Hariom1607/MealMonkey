cb80c8d1b5403cf917870c4f37d0503693c5a602
Hariom-8253 <hariom.sharma@brainvire.com>
2025-07-31 18:15:58 +0530

    Created a new food Delivery App and Splash Screen in it.

--- MealMonkey/Module/Login Screen/ViewController/LoginViewController.swift
+++ MealMonkey/Module/Login Screen/ViewController/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  MealMonkey
 //
 //  Created by Hariom Sharma on 31/07/25.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
-
 }
-
---
+++ MealMonkey/Module/Splash Screen/ViewController/SplashViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SplashViewController.swift
+//  MealMonkey
+//
+//  Created by Hariom Sharma on 31/07/25.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        self.navigationController?.navigationBar.isHidden = true
+        sleep(3)
+
+        
+        let storyboard = UIStoryboard(name: "LoginStoryboard", bundle: nil)
+        if let mlvc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController{
+            self.navigationController?.pushViewController(mlvc, animated: true)
+            
+        }
+    }
+
+
+}
